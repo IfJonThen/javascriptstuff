0f0a49bf11ec2110f37cd9c8dce9aee169365706
jonsenY <pikahatonjon@gmail.com>
2017-02-01 20:37:00 -0800

    added buttons

--- js/boxes.js
+++ js/boxes.js
@@ -1,3 +1,17 @@
 /**
  * Created by Jonsen on 2/1/2017.
  */
+$(function(){
+  $(".box").on('click', ()=>{console.log($('.btn').text())});
+  $(".btn").on('click',(event)=>{
+    let t = $("#box1")[0].classList;
+    console.log(t);
+
+    // let t = $("#box1")[0].classList;
+    t.add(".mbox");
+    console.log(t);
+
+  });
+
+
+});
